75500e01e6555309e8b3cbd1e41a88858e93af30
Angelik Operio <122664874+LalaFC@users.noreply.github.com>
2025-03-12 19:26:28 +0800

    NavMesh Link with Animation

--- Assets/Scripts/AIRootMotionCrtler.cs
+++ Assets/Scripts/AIRootMotionCrtler.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
@@ -7,6 +8,7 @@ public class AIRootMotionCrtler : MonoBehaviour
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private Animator animator;
     [SerializeField] private float maxTarget;
+    [SerializeField] private bool isOffNavmesh;
 
     private void OnValidate()
     {
@@ -17,19 +19,30 @@ public class AIRootMotionCrtler : MonoBehaviour
     {
         if (agent.hasPath)
         {
-            var dir = (agent.steeringTarget - transform.position).normalized;
-            var aniDir = transform.InverseTransformDirection(dir);
-            var isFacingMoveDirection = Vector2.Dot(dir, transform.forward) > .5f;
-
-            animator.SetFloat("Horizontal", isFacingMoveDirection ? aniDir.x : 0, 0.5f, Time.deltaTime);
-            animator.SetFloat("Vertical", isFacingMoveDirection ? aniDir.z : 0, 0.5f, Time.deltaTime);
-
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(dir), maxTarget*Time.deltaTime);
-
-            if (Vector3.Distance(transform.position, agent.destination) < agent.radius)
+            if (agent.isOnOffMeshLink)
             {
-                agent.ResetPath();
+                isOffNavmesh = true;
+                var link = agent.currentOffMeshLinkData;
+                StartCoroutine(DoOffMeshLink(link));
+
             }
+            else
+            {
+                var dir = (agent.steeringTarget - transform.position).normalized;
+                var aniDir = transform.InverseTransformDirection(dir);
+                var isFacingMoveDirection = Vector2.Dot(dir, transform.forward) > .5f;
+
+                animator.SetFloat("Horizontal", isFacingMoveDirection ? aniDir.x : 0, 0.5f, Time.deltaTime);
+                animator.SetFloat("Vertical", isFacingMoveDirection ? aniDir.z : 0, 0.5f, Time.deltaTime);
+
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(dir), maxTarget * Time.deltaTime);
+
+                if (Vector3.Distance(transform.position, agent.destination) < agent.radius)
+                {
+                    agent.ResetPath();
+                }
+            }
+
         }
         else
         {
@@ -47,10 +60,26 @@ public class AIRootMotionCrtler : MonoBehaviour
             }
         }
     }
+    IEnumerator DoOffMeshLink(OffMeshLinkData link)
+    {
+        animator.CrossFade("Jump", 0.2f);
+        var time = 0.7f;
+        var totalTime = time;
+
+        while (time > 0)
+        {
+            time = Mathf.Max(0, time - Time.deltaTime);
+            transform.position = Vector3.Lerp(link.startPos, link.endPos, 1-time / totalTime);
+            yield return new WaitForSeconds(0);
+        }
+        agent.CompleteOffMeshLink();
+    }
+
     private void OnDrawGizmos()
     {
         if (agent.hasPath) 
         {
+
             for (int i = 0; i<agent.path.corners.Length-1; i++)
             {
                 Debug.DrawLine(agent.path.corners[1], agent.path.corners[i+1], Color.red);
